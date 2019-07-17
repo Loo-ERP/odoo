61e59c9a61434d6d39bdc77455416e1ca6e0c68c
Carlos Lopez <celm1990@hotmail.com>
2019-07-16 22:30:13 -0500

    IMP: notificar usando plantilla de correo estandar

--- addons/sale/models/sale.py
+++ addons/sale/models/sale.py
@@ -687,7 +687,7 @@ class SaleOrder(models.Model):
             if email_act and email_act.get('context'):
                 email_ctx = email_act['context']
                 email_ctx.update(default_email_from=order.company_id.email)
-                order.with_context(**email_ctx).message_post_with_template(email_ctx.get('default_template_id'))
+                order.with_context(**email_ctx).message_post_with_template(email_ctx.get('default_template_id'), **{'notif_layout': 'mail.mail_notification_paynow'})
         return True
 
     @api.multi
