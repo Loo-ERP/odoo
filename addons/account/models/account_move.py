8bd6152f197bcc38460b76fddf4fb6b4e8f7bf83
Carlos Lopez <celm1990@hotmail.com>
2019-08-08 19:16:09 -0500

    IMP: facilitar herencia en modulos personalizados, poder usar otra secuencia para generar el numero de asiento contable, esto para evitar errores de concurrencia

--- addons/account/models/account_move.py
+++ addons/account/models/account_move.py
@@ -282,6 +282,27 @@ class AccountMove(models.Model):
         else:
             res = super(AccountMove, self).write(vals)
         return res
+    
+    @api.multi
+    def _get_name_for_move(self, invoice=False):
+        journal = self.journal_id
+        new_name = False
+        if invoice and invoice.move_name and invoice.move_name != '/':
+            new_name = invoice.move_name
+        else:
+            if journal.sequence_id:
+                # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
+                sequence = journal.sequence_id
+                if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
+                    if not journal.refund_sequence_id:
+                        raise UserError(_('Please define a sequence for the credit notes'))
+                    sequence = journal.refund_sequence_id
+
+                new_name = sequence.with_context(ir_sequence_date=self.date).next_by_id()
+            else:
+                raise UserError(_('Please define a sequence on the journal.'))
+        return new_name
+        
 
     @api.multi
     def post(self, invoice=False):
@@ -290,23 +311,7 @@ class AccountMove(models.Model):
         self.mapped('line_ids').create_analytic_lines()
         for move in self:
             if move.name == '/':
-                new_name = False
-                journal = move.journal_id
-
-                if invoice and invoice.move_name and invoice.move_name != '/':
-                    new_name = invoice.move_name
-                else:
-                    if journal.sequence_id:
-                        # If invoice is actually refund and journal has a refund_sequence then use that one or use the regular one
-                        sequence = journal.sequence_id
-                        if invoice and invoice.type in ['out_refund', 'in_refund'] and journal.refund_sequence:
-                            if not journal.refund_sequence_id:
-                                raise UserError(_('Please define a sequence for the credit notes'))
-                            sequence = journal.refund_sequence_id
-
-                        new_name = sequence.with_context(ir_sequence_date=move.date).next_by_id()
-                    else:
-                        raise UserError(_('Please define a sequence on the journal.'))
+                new_name = move._get_name_for_move(invoice=invoice)
 
                 if new_name:
                     move.name = new_name
